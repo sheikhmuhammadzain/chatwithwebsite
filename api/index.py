388c94b082553b9b5a0249160f62044ad72d2ac5
sheikhmuhammadzain <zaingee320@gmail.com>
2025-04-30 22:15:53 +0500

    adeed vercel

---
+++ api/index.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI, Request
+import sys
+import os
+
+# Add the parent directory to sys.path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+# Import the FastAPI app from main.py
+from main import app
+
+# Export the app for Vercel serverless function 
